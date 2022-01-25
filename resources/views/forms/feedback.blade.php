3c43ac357be95ee23fa55607598a48c3778d5db1
anton <aasimonov1994@mail.ru>
2022-01-25 22:15:50 +0300

    Homework5

---
+++ resources/views/forms/feedback.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+@section('news')
+    <div class="container" style="width:1000px;margin: 0 auto;">
+        <h1 class="display-3">Форма обратной связи</h1>
+    </div>
+@endsection
+
+@section('content')
+
+    <form method="post" style="width:500px;margin: 0 auto;" action="{{route('forms.index.store')}}">
+        @csrf
+        <div class="form-group" style="width: 400px;">
+            <label for="title">Имя пользователя</label>
+            <input type="text" class="form-control" id="title" name="title" required>
+        </div>
+        <div class="form-group" style="width: 400px;">
+            <label for="description">Комментарий</label>
+            <textarea class="form-control" name="description" id="description"></textarea>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-success" style="">Сохранить</button>
+    </form>
+@endsection
