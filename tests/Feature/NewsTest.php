af5a769e89f8e84d307c83f182fb7271d51a5702
anton <aasimonov1994@mail.ru>
2022-01-26 22:56:05 +0300

    Homework4

---
+++ tests/Feature/NewsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NewsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testCatalogAvailable()
+    {
+        $response = $this->get('/catalog');
+
+        $response->assertStatus(200);
+    }
+
+    public function testNewsAvailable()
+    {
+        $response = $this->get('/catalog/news');
+
+        $response->assertStatus(200);
+    }
+    public function testFormsOrderAvailable()
+    {
+        $response = $this->get('/order');
+
+        $response->assertStatus(200);
+    }
+    public function testFormsFeedbackAvailable()
+    {
+        $response = $this->get('/feedback');
+
+        $response->assertStatus(200);
+    }
+}
